fc8949b2b8aa8738fe224b4a518b060e0a97f052
DCrawley94 <76622730+DCrawley94@users.noreply.github.com>
2022-06-21 09:22:33 +0100

    Update food.js

--- food.js
+++ food.js
@@ -1,3 +1,9 @@
 const haveSomeFoodAndDrinks = () => {
-	console.log("pirates eating biscuits dunked into rum");
+	console.log("eat biscuits");
+	console.log("drink rum")
+	console.log("feed the parrots")
 };
+
+
+
+
